3fc938c08824bf457ce614d4ba3f896e488e4b34
siddheshnm <87862103+siddheshnm@users.noreply.github.com>
2022-11-22 18:10:48 +0530

    Delete MerchantAndProduct directory

--- MerchantAndProduct/src/main/java/com/multiclientwebsite/merchantandproduct/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.multiclientwebsite.merchantandproduct.repository;
-
-import com.multiclientwebsite.merchantandproduct.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- MerchantAndProduct/src/main/java/com/multiclientwebsite/merchantandproduct/repository/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.multiclientwebsite.merchantandproduct.repository;
-
-import com.multiclientwebsite.merchantandproduct.entity.Merchant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MerchantRepository extends JpaRepository<Merchant, Long> {
-}
